f8f8b2e9047269ff356d9fd41f12e2e5956deff4
jsolejr <jsolejr@gmail.com>
2023-05-11 19:47:33 -0500

    Update MWLQueryTool.py
    Added modifications to create a result window that can scroll so large result sets don't go beyond screen edges.
    
    Signed-off-by: jsolejr <jsolejr@gmail.com>

--- MWLQueryTool.py
+++ MWLQueryTool.py
@@ -1,86 +1,67 @@
-import sys
-from tkinter import simpledialog, Tk, messagebox
-from pydicom.dataset import Dataset
-from pynetdicom import AE, evt, build_role, debug_logger
+from pynetdicom import AE
 from pynetdicom.sop_class import ModalityWorklistInformationFind
+import tkinter as tk
+from tkinter import messagebox, simpledialog, Text, Scrollbar, Toplevel
+import os
+from pydicom.dataset import Dataset
 
-# Set up logging
-debug_logger()
-
-# Create a dialog box for user input
-root = Tk()
+# Create a simple GUI to prompt the user for inputs
+root = tk.Tk()
 root.withdraw()  # Hide the main window
 
-calling_ae_title = simpledialog.askstring("Input", "Enter Calling AE Title:")
-ae_title = simpledialog.askstring("Input", "Enter Destination AE Title:")
-ip_address = simpledialog.askstring("Input", "Enter PACS Server IP:")
-port = simpledialog.askinteger("Input", "Enter PACS Server Port:")
+# Prompt the user for the calling AE, called AE, remote IP and remote port
+calling_ae = simpledialog.askstring("Input", "Please enter the calling AE:")
+called_ae = simpledialog.askstring("Input", "Please enter the called AE:")
+remote_ip = simpledialog.askstring("Input", "Please enter the remote IP:")
+remote_port = simpledialog.askinteger("Input", "Please enter the remote port:")
 
-# Initialize the Application Entity (AE)
-ae = AE(ae_title=calling_ae_title)
+# Create a dataset representing a worklist
+dataset = Dataset()
+
+# Prompt the user to modify the DICOM Query Attributes
+dataset.PatientName = simpledialog.askstring("Input", "Please enter the Patient Name:")
+dataset.StudyDate = simpledialog.askstring("Input", "Please enter the Study Date:")
+dataset.Modality = simpledialog.askstring("Input", "Please enter the Modality:")
+dataset.PatientID = simpledialog.askstring("Input", "Please enter the Patient ID:")
+
+# Initialize the Application Entity and specify the SOP class
+ae = AE(ae_title=calling_ae)
 ae.add_requested_context(ModalityWorklistInformationFind)
 
-# Create the query dataset
-query = Dataset()
-query.PatientName = ''
-query.StudyDate = ''
-query.Modality = 'CT'  # Set the modality you want to search for
-query.AccessionNumber = ''
-query.PatientID = ''
-query.ScheduledProcedureStepDescription = ''
+# Establish the association
+assoc = ae.associate(remote_ip, remote_port, ae_title=called_ae)
 
-# Check if the user wants to provide query information
-if messagebox.askyesno("Input", "Would you like to provide query information?"):
-    query.PatientName = simpledialog.askstring("Input", "Enter Patient Name:")
-    query.StudyDate = simpledialog.askstring(
-        "Input", "Enter Study Date (YYYYMMDD):")
-    query.Modality = simpledialog.askstring("Input", "Enter Modality:")
-    query.AccessionNumber = simpledialog.askstring(
-        "Input", "Enter Accession Number:")
-    query.PatientID = simpledialog.askstring("Input", "Enter Patient ID:")
-    query.ScheduledProcedureStepDescription = simpledialog.askstring(
-        "Input", "Enter Scheduled Procedure Step Description:")
+# Save the results in a list
+results = []
 
-# Define the C-FIND request event handler
-
-
-def handle_find(event):
-    ds = event.identifier
-
-    # Check if the response includes the Status element
-    if 'Status' not in ds:
-        return
-
-    # Check the status of the C-FIND response
-    status = ds.Status
-
-    if status == 0xFF00:
-        with open('output.txt', 'a') as f:
-            f.write("Pending response received:\n")
-            f.write(str(ds))
-            f.write("\n\n")
-        print("Pending response received:")
-        print(ds)
-    elif status in (0x0000, 0xFF00, 0xFF01):
-        print("Success, C-FIND operation completed")
-    else:
-        print(f"Error, C-FIND operation failed with status: {status}")
-
-
-# Associate the C-FIND event handler with the event
-handlers = [(evt.EVT_C_FIND, handle_find)]
-
-# Set up the remote PACS server information
-assoc = ae.associate(ip_address, port, ae_title=ae_title, ext_neg=[
-                     build_role(ModalityWorklistInformationFind)], evt_handlers=handlers)
-
+# Send the C-FIND request
 if assoc.is_established:
-    # Send the C-FIND request
-    response = assoc.send_c_find(query, ModalityWorklistInformationFind)
+    responses = assoc.send_c_find(dataset, ModalityWorklistInformationFind)
 
-    # Wait for the association to be released
+    # Iterate over the responses
+    for (status, identifier) in responses:
+        if status:
+            print("C-FIND response received with status", status)
+            # Save the result
+            results.append(identifier)
+
+    # Release the association
     assoc.release()
-else:
-    error_msg = f"Failed to establish association with remote PACS server: {ip_address}:{port}"
-    print(error_msg)
-    messagebox.showerror("Error", error_msg)
+
+# Create a new window to display the results
+results_window = Toplevel(root)
+results_window.title("Results")
+
+# Create a scrollable text area
+text_area = Text(results_window, wrap="word")
+text_area.pack(side="left", fill="both", expand=True)
+scrollbar = Scrollbar(results_window, command=text_area.yview)
+scrollbar.pack(side="right", fill="y")
+text_area['yscrollcommand'] = scrollbar.set
+
+# Insert the results into the text area
+for result in results:
+    text_area.insert("end", str(result) + "\n")
+
+# Show the window
+results_window.mainloop()
